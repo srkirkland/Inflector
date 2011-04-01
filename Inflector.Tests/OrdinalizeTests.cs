a7818f347c0bfba04c6d068e3e822771a230699d
David Kemp <David.Kemp@brilliantmedia.co.uk>
2011-04-01 15:29:49 +0100

    removed redundant testing code

--- Inflector.Tests/OrdinalizeTests.cs
+++ Inflector.Tests/OrdinalizeTests.cs
@@ -8,19 +8,12 @@ namespace Inflector.Tests
     [TestFixture]
     public class OrdinalizeTests : InflectorTestBase
     {
-        private readonly Dictionary<int, string> TestDataNumbers;
-
         [Test]
         public void Ordinalize()
         {
             foreach (var pair in TestData)
             {
                 Assert.AreEqual(pair.Key.Ordinalize(), pair.Value);
-            }
-
-            foreach (var testDataNumber in TestDataNumbers)
-            {
-                Assert.AreEqual(testDataNumber.Key.Ordinalize(), testDataNumber.Value);
             }
         }
 
@@ -87,40 +80,6 @@ namespace Inflector.Tests
             TestData.Add("110", "110th");
             TestData.Add("1000", "1000th");
             TestData.Add("1001", "1001st");
-
-            TestDataNumbers =
-                new Dictionary<int, string>
-                    {
-                        {0, "0th"},
-                        {1, "1st"},
-                        {2, "2nd"},
-                        {3, "3rd"},
-                        {4, "4th"},
-                        {5, "5th"},
-                        {6, "6th"},
-                        {7, "7th"},
-                        {8, "8th"},
-                        {9, "9th"},
-                        {10, "10th"},
-                        {11, "11th"},
-                        {12, "12th"},
-                        {13, "13th"},
-                        {14, "14th"},
-                        {20, "20th"},
-                        {21, "21st"},
-                        {22, "22nd"},
-                        {23, "23rd"},
-                        {24, "24th"},
-                        {100, "100th"},
-                        {101, "101st"},
-                        {102, "102nd"},
-                        {103, "103rd"},
-                        {104, "104th"},
-                        {110, "110th"},
-                        {1000, "1000th"},
-                        {1001, "1001st"},
-                    };
-
         }
     }
 }
